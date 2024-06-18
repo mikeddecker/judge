5045477a4c1b28f3431850ee0ab44b9131d21c3b
mike.dedecker <mike.dedecker@student.hogent.be>
2024-06-18 22:38:51 +0200

    seq labels

--- scripts/remove_last_5_labels.py
+++ scripts/remove_last_5_labels.py
@@ -6,6 +6,7 @@ df_labels = pickle_load_or_create(video_border_labels_path, lambda x: {})
 
 print('before delete')
 print(df_labels.loc[len(df_labels)-7:])
-pickle_save(video_border_labels_path, df_labels.loc[:len(df_labels)-5])
+df_labels = df_labels.loc[:len(df_labels)-5]
+pickle_save(video_border_labels_path, df_labels)
 print('after delete')
 print(df_labels.loc[len(df_labels)-2:])
