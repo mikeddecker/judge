e82a1f8372389da9b8c04d88e0476ae1f71d56c7
mike.dedecker <mike.dedecker@student.hogent.be>
2024-06-21 12:57:10 +0200

    inserting labels into db (notebook)

--- db/init.sql
+++ db/init.sql
@@ -1,28 +1,33 @@
 CREATE DATABASE IF NOT EXISTS judge;
 
+DROP TABLE IF EXISTS FrameLabels;
+DROP TABLE IF EXISTS Videos;
+DROP TABLE IF EXISTS Folders;
+
 CREATE TABLE Folders (
-    FolderID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    Path NVARCHAR(50) NOT NULL
+    folderID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    path NVARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Videos (
-    VideoID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    FolderID INT NOT NULL,
-    Name NVARCHAR(255) NOT NULL,
-    Training BOOLEAN NOT NULL,
-    Obstruction TINYINT NOT NULL, -- 0 geen - 100 high obstruction
-    Quality TINYINT NOT NULL, -- 0 zeer wazig - 100 goed
-    DimensionX SMALLINT NOT NULL, -- 1920 of 1080
-    DimensionY SMALLINT NOT NULL, -- 1080 of 1920 of 3k...
+    videoID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    folderID INT NOT NULL,
+    name NVARCHAR(255) NOT NULL,
+    training BOOLEAN NOT NULL,
+    obstruction TINYINT NOT NULL, -- 0 geen - 100 high obstruction
+    quality TINYINT NOT NULL, -- 0 zeer wazig - 100 goed
+    type TINYINT NOT NULL, -- 0 = competitie, 1 = distant, sporthal-like, 2 = dicht & vrij, 3 = distant, sporthallike, ruis, 4 = dicht & achtergrondruis
+    dimensionX SMALLINT NOT NULL, -- 1920 of 1080
+    dimensionY SMALLINT NOT NULL, -- 1080 of 1920 of 3k...
 
-    FOREIGN KEY (FolderID) REFERENCES Folders(FolderID)
+    FOREIGN KEY (folderID) REFERENCES Folders(folderID)
 );
 
 CREATE TABLE FrameLabels (
-    VideoID INT NOT NULL,
-    FrameNr SMALLINT NOT NULL,
-    Label TINYINT NOT NULL, -- 0 - 9 currently (on ground, air, release, power...)
-    Verrified BOOLEAN NOT NULL, -- 0 not, 1
+    videoID INT NOT NULL,
+    frameNr SMALLINT NOT NULL,
+    label TINYINT NOT NULL, -- 0 - 9 currently (on ground, air, release, power...)
+    manual_insert BOOLEAN NOT NULL, -- 0 not, 1 manual
     
-    FOREIGN KEY (VideoID) REFERENCES Videos(VideoID)
-);
+    FOREIGN KEY (videoID) REFERENCES Videos(videoID)
+)
