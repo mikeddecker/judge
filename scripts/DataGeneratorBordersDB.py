1897d1ea2a0b5dcbb983a1fde6619c5359062bbf
mike.dedecker <mike.dedecker@student.hogent.be>
2024-06-24 13:32:02 +0200

    skillborders model 2 full epochs

--- scripts/DataGeneratorBordersDB.py
+++ scripts/DataGeneratorBordersDB.py
@@ -32,22 +32,24 @@ class DataGeneratorSkillBorders(keras.utils.Sequence):
     def __len__(self):
         'Denotes the number of batches per epoch'
         if self.len is None:
-            self.len = self.repo.get_randomized_borderlabels_and_batches_per_video(batch_size=self.batch_size, training=True).iloc[-1]['total_batches']
-            print('len called', 'len is:', self.len)
+            self.len = self.repo.get_randomized_borderlabels_and_batches_per_video(batch_size=self.batch_size, training=self.train).iloc[-1]['total_batches']
+        print('len called', 'len is:', self.len)
         return self.len
 
     def __getitem__(self, batch_nr):
         'Generate one batch of data'
         # Generate batch df view
         df = self.batch_order
+        print(' getitem, batch_nr', batch_nr, 'train is:', self.train)
         index = len(df[df['total_batches']<=batch_nr])
         video_id = df.iloc[index]['videoID']
         video_batch_nr = batch_nr - (df.iloc[index-1]['total_batches'] if index > 0 else 0)
 
-        print('vid_id_idx', index, 'vid_batch_nr', video_batch_nr)
+        # print(batch_nr, ' vidID', video_id, '& vid_batch_nr', video_batch_nr)
 
-        
         df_labels = self.repo.get_borderlabels_batch(videoID=video_id, batch_nr=video_batch_nr, batch_size=self.batch_size)
+        if (len(df_labels) < 20):
+            df_labels = self.fill_batch_size_dimension(df_labels)
 
         y = np.array(df_labels['label'])
         y = np.where(y == 9, 3, y)
@@ -56,7 +58,7 @@ class DataGeneratorSkillBorders(keras.utils.Sequence):
         y = np.expand_dims(y, axis=-1)
         
         min_frame = df_labels.iloc[0]['frameNr']
-        max_frame = df_labels.iloc[-1]['frameNr']
+        max_frame = min_frame + self.batch_size - 1
         path = '../' + self.repo.get_path(video_id)
         X = get_frames(path, min_frame, max_frame, dim=self.dim)
 
@@ -64,12 +66,26 @@ class DataGeneratorSkillBorders(keras.utils.Sequence):
         
         return X, y
 
+    def fill_batch_size_dimension(self, df_labels):
+        print('fill_batch_size_dimension_called')
+        min_frame = df_labels.iloc[-1]['frameNr'] + 1
+        max_frame_exlcuded = df_labels.iloc[0]['frameNr'] + self.batch_size
+        arr = np.arange(min_frame, max_frame_exlcuded)
+        df_fill = pd.DataFrame({
+            'frameNr': [7777 for i in arr],
+            'label': [0 for i in arr]
+        })
+        return pd.concat([df_labels, df_fill])
+
     def on_epoch_end(self):
+        print('on_epoch_end_called')
         'Updates indexes after each epoch'
         # shuffle, insert indexes remain, position in df changes
         self.batch_order = self.repo.get_randomized_borderlabels_and_batches_per_video(self.batch_size, self.train)
+        print(self.batch_order)
 
     def load_frame(self, path, frame_nr, dx, dy):
+        print('load_frame_called')
         cap = cv2.VideoCapture(self.videofolder + path)
         cap.set(cv2.CAP_PROP_POS_FRAMES, frame_nr)
         res, frame = cap.read()
