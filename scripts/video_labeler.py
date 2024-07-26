44eb43df08f799ff0e99d8eb788cb970b5340999
mike.dedecker <mike.dedecker@student.hogent.be>
2024-07-26 14:28:48 +0200

    videolabeler layout & resizable

--- scripts/video_labeler.py
+++ scripts/video_labeler.py
@@ -6,27 +6,64 @@ from PIL import Image, ImageTk
 from DataRepository import DataRepository
 from DataGeneratorFrames import DataGeneratorRectangles
 
+# -------------------------- DEFINING GLOBAL VARIABLES -------------------------
+# these are just the colors we'll be using to paint our rooms (frames)
+selectionbar_color = '#eff5f6'
+sidebar_color = '#F5E1FD'
+header_color = '#53366b'
+visualisation_frame_color = "#ffffff"
+
+
 class VideoLabeler:
-    def __init__(self, root, video_id):
+    def __init__(self, root, video_id, batch_size, predict=False):
+        # --- Video config & Y values ---
         self.repo = DataRepository()
         self.video_id = video_id
         self.video_path = '../' + self.repo.get_path(video_id)
-        self.framelbls_and_rects = self.repo.query_framelabels(video_id)
+        self.y_frames = self.repo.query_framelabels(video_id) # frame list: rectangle, skillstatus
+        self.y_skills = self.repo.get_borders(video_id)  # Skill-list: toad, start = 100, end = 120
 
+        if predict:
+            # TODO : load y predicted values from models.
+            pass
+
+        # --- Prepare screen ---
+
+        # --- BASIC APP LAYOUT ---
         self.root = root
         self.root.title(self.video_path)
+        self.root.geometry("1100x700")
+        self.root.config(background=selectionbar_color)
+        # icon = tk.PhotoImage(file='images\\WU_logo.png') # Icon for header on top
+        # self.iconphoto(True, icon)
 
+        # --- HEADER ---
+        self.header = tk.Frame(root, bg=header_color)
+        self.header.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.04)
+
+        # --- SIDEBAR ---
+        # SIDEBAR FRAME
+        self.sidebar = tk.Frame(root, bg=sidebar_color)
+        self.sidebar.place(relx=0, rely=0, relwidth=0.2, relheight=1)
+
+        # -- Mainframe ---
+        self.main_frame = tk.Frame(root)
+        self.main_frame.place(relx=0.2, rely=0.04, relwidth=0.8, relheight=0.84)
+
+        # --- Footer ---
+        # Slider & buttons
+        self.footer = tk.Frame(root)
+        self.footer.place(relx=0.2, rely=0.88, relwidth=0.8, relheight=0.12)
+
+
+        # --- Loading video ---
         self.cap = cv2.VideoCapture(self.video_path)
-        print('framecount: ', self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
         self.original_width = self.cap.get(3)  # float `width`
         self.original_height = self.cap.get(4)  # float `height`
-        if self.original_height > self.original_width:
-            self.display_height = 850
-            self.display_width = self.original_width / (self.original_height / 850)
-        else:
-            self.display_width = 1200
-            self.display_height = self.original_height / (self.original_width / 1200)
-        self.current_pos = 0
+        self.update_framesize()
+        
+
+        self.current_frameNr = 0
         
         self.rectangles = True
 
@@ -38,11 +75,11 @@ class VideoLabeler:
                 'resize' : self.resize_rectangle,
                 'watch' : lambda x, y: None,
             }
-            self.frame_label = tk.Label(root)
+            self.frame_label = tk.Label(self.main_frame)
             self.frame_label.pack()
             self.frame_label.bind("<Button-1>", self.on_click)
             
-        first_frame = self.framelbls_and_rects.loc[0]
+        first_frame = self.y_frames.loc[0]
         print(first_frame)
         print((0.5 if first_frame['rect_center_x'] is None else first_frame['rect_center_x']))
         self.rect_center_x = int((0.5 if first_frame['rect_center_x'] is None else first_frame['rect_center_x']) * self.original_width)
@@ -50,28 +87,27 @@ class VideoLabeler:
         self.rect_size = 1 if first_frame['rect_size'] is None else first_frame['rect_size']
 
         # Custom slider using Canvas
-        self.slider_canvas = tk.Canvas(root, height=30, bg='white')
+        self.slider_canvas = tk.Canvas(self.footer, height=30, bg='white')
         self.slider_canvas.pack(fill=tk.X, expand=1)
         self.slider_canvas.bind('<Button-1>', self.slider_clicked)
         self.slider_canvas.bind('<B1-Motion>', self.slider_dragged)
         self.slider_canvas.bind('<MouseWheel>', self.slider_zoomed)
         
-        self.play_button = tk.Button(root, text="Play", command=self.play_video)
+        self.play_button = tk.Button(self.footer, text="Play", command=self.play_video)
         self.play_button.pack(side=tk.LEFT, padx=10)
         
-        self.pause_button = tk.Button(root, text="Pause", command=self.pause_video)
+        self.pause_button = tk.Button(self.footer, text="Pause", command=self.pause_video)
         self.pause_button.pack(side=tk.LEFT, padx=10)
 
-        self.select_start_button = tk.Button(root, text="Border Start", command=self.select_start)
+        self.select_start_button = tk.Button(self.footer, text="Border Start", command=self.select_start)
         self.select_start_button.pack(side=tk.LEFT, padx=10)
         
-        self.select_end_button = tk.Button(root, text="Border End", command=self.select_end)
+        self.select_end_button = tk.Button(self.footer, text="Border End", command=self.select_end)
         self.select_end_button.pack(side=tk.LEFT, padx=10)
         
         self.selected_start_frame = None
         self.selected_end_frame = None
         
-        self.selected_ranges = self.repo.get_borders(video_id)  # List to store selected ranges
 
         self.playing = True
         self.zoom_factor = 1.0  # Initial zoom factor
@@ -90,6 +126,8 @@ class VideoLabeler:
         self.root.bind('<t>', self.toggle_textbox)
         self.root.bind('<y>', self.reset_rectangle)
         self.root.bind('<space>', self.toggle_play_pause)
+        root.bind("<Configure>", lambda event: self.update_framesize())
+
         
         if self.rectangles:
             self.playing = False
@@ -98,10 +136,18 @@ class VideoLabeler:
 
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def update_framesize(self):
+        self.root.update()
+        prop_relx = self.main_frame.winfo_width() / self.original_width
+        prop_rely = self.main_frame.winfo_height() / self.original_height
+        smallest_rel_size = min(prop_relx, prop_rely)
+        self.display_height = int(smallest_rel_size * self.original_height)
+        self.display_width = int(smallest_rel_size * self.original_width)
     
     def show_frame(self):
         ret, frame = self.cap.read()
-        self.current_pos = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+        self.current_frameNr = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
 
         if ret:
             if self.current_modus == 'watch':
@@ -161,7 +207,7 @@ class VideoLabeler:
         print(f"Clicked at ({event.x}, {event.y}), corresponding to ({original_x}, {original_y}) in original frame")
 
         self.get_textbox_function()(original_x, original_y)
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_pos - 1)
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frameNr - 1)
 
         if not self.playing:
             self.show_frame()
@@ -178,8 +224,8 @@ class VideoLabeler:
     def slider_clicked(self, event):
         total_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
         visible_frames = self.visible_range_end - self.visible_range_start
-        self.current_pos = int((event.x / self.slider_canvas.winfo_width()) * visible_frames + self.visible_range_start)
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_pos)
+        self.current_frameNr = int((event.x / self.slider_canvas.winfo_width()) * visible_frames + self.visible_range_start)
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frameNr)
         ret, frame = self.cap.read()
         if ret:
             frame = self.resize_frame(frame)
@@ -210,31 +256,31 @@ class VideoLabeler:
     def adjust_visible_range(self):
         total_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
         visible_frames = total_frames / self.zoom_factor
-        center_frame = self.current_pos
+        center_frame = self.current_frameNr
         
         # Calculate the start and end of the visible range to center the current frame
         self.visible_range_start = max(center_frame - visible_frames // 2, 0)
         self.visible_range_end = min(center_frame + visible_frames // 2, total_frames)
         
         # Adjust current position if out of new visible range
-        if self.current_pos < self.visible_range_start:
-            self.current_pos = self.visible_range_start
-        if self.current_pos > self.visible_range_end:
-            self.current_pos = self.visible_range_end
+        if self.current_frameNr < self.visible_range_start:
+            self.current_frameNr = self.visible_range_start
+        if self.current_frameNr > self.visible_range_end:
+            self.current_frameNr = self.visible_range_end
         
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_pos)
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frameNr)
 
     def update_slider(self):
         self.slider_canvas.delete("all")
         total_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
         visible_frames = self.visible_range_end - self.visible_range_start
-        slider_pos = ((self.current_pos - self.visible_range_start) / visible_frames) * self.slider_canvas.winfo_width()
+        slider_pos = ((self.current_frameNr - self.visible_range_start) / visible_frames) * self.slider_canvas.winfo_width()
         
         # Draw slider background
         self.slider_canvas.create_rectangle(0, 0, self.slider_canvas.winfo_width(), 30, fill='white')
         
         # Draw selected ranges
-        for idx, row in self.selected_ranges[['frame_start', 'frame_end']].iterrows():
+        for idx, row in self.y_skills[['frame_start', 'frame_end']].iterrows():
             start = row['frame_start']
             end = row['frame_end']
             
@@ -270,7 +316,7 @@ class VideoLabeler:
 
     def previous_frame(self, event):
         if not self.playing:
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_pos - 2)
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frameNr - 2)
             self.show_frame()
 
     def select_start_key(self, event):
@@ -288,9 +334,9 @@ class VideoLabeler:
         if self.selected_start_frame is not None and self.selected_end_frame is not None:
             if self.repo.is_valid_border(self.video_id, self.selected_start_frame, self.selected_end_frame):
                 print('valid')
-                idx = len(self.selected_ranges)
+                idx = len(self.y_skills)
                 self.repo.add_border(self.video_id, self.selected_start_frame, self.selected_end_frame, 1)
-                self.selected_ranges.loc[idx] = [self.video_id, self.selected_start_frame, self.selected_end_frame, 1]
+                self.y_skills.loc[idx] = [self.video_id, self.selected_start_frame, self.selected_end_frame, 1]
                 self.selected_start_frame = None
                 self.selected_end_frame = None
                 print(f"Selected range: {2}")
@@ -300,22 +346,22 @@ class VideoLabeler:
                 self.selected_end_frame = None
 
     def remove_border(self, event):
-        df_skill = self.selected_ranges[(self.selected_ranges['frame_start'] <= self.current_pos) & (self.current_pos <= self.selected_ranges['frame_end'])]
+        df_skill = self.y_skills[(self.y_skills['frame_start'] <= self.current_frameNr) & (self.current_frameNr <= self.y_skills['frame_end'])]
         if len(df_skill) > 0:
             self.repo.remove_border(self.video_id, df_skill.iloc[0]['frame_start'], df_skill.iloc[0]['frame_end'])
-            self.selected_ranges = self.repo.get_borders(self.video_id)        
+            self.y_skills = self.repo.get_borders(self.video_id)        
 
     def on_closing(self, event=None):
         self.playing = False
         
-        print(self.selected_ranges)
-        print(self.framelbls_and_rects)
+        print(self.y_skills)
+        print(self.y_frames)
         
         self.cap.release()
         self.root.destroy()
 
     def create_readonly_textbox(self, initial_text):
-        text_box = tk.Text(root, wrap='word', height=1)
+        text_box = tk.Text(self.header, wrap='word', height=1)
         text_box.insert('1.0', initial_text)  # Insert the initial text at the beginning
         text_box.config(state='disabled')    # Make the text box read-only
         text_box.pack()
@@ -354,8 +400,8 @@ class VideoLabeler:
 
     def follow_rectangle(self):
         # just needs two params
-        print('current pos: ', self.current_pos)
-        curr_frame = self.framelbls_and_rects.loc[self.current_pos-1]
+        print('current pos: ', self.current_frameNr)
+        curr_frame = self.y_frames.loc[self.current_frameNr-1]
         if not self.is_non_or_nan(curr_frame['rect_size']):
             self.rect_center_x = int(curr_frame['rect_center_x'] * self.original_width)
             self.rect_center_y = int(curr_frame['rect_center_y'] * self.original_height)
@@ -366,7 +412,7 @@ class VideoLabeler:
         print('rectangle moved', x, y)
         self.rect_center_x = int(x)
         self.rect_center_y = int(y)
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_pos - 1)
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frameNr - 1)
     
     def resize_rectangle(self, x, y):
         print('rectangle resized')
@@ -380,18 +426,19 @@ class VideoLabeler:
             self.rect_size *= 1.03
     
     def update_framelabels_rectangles(self):
-        print('update label ', self.current_pos)
+        print('update label ', self.current_frameNr)
         rel_x = self.rect_center_x / self.original_width
         rel_y = self.rect_center_y / self.original_height
-        self.framelbls_and_rects.loc[self.current_pos-1, 'rect_center_x'] = rel_x
-        self.framelbls_and_rects.loc[self.current_pos-1, 'rect_center_y'] = rel_y
-        self.framelbls_and_rects.loc[self.current_pos-1, 'rect_size'] = self.rect_size
-        self.repo.update_rectangle(self.video_id, self.current_pos, rel_x, rel_y, self.rect_size)
+        self.y_frames.loc[self.current_frameNr-1, 'rect_center_x'] = rel_x
+        self.y_frames.loc[self.current_frameNr-1, 'rect_center_y'] = rel_y
+        self.y_frames.loc[self.current_frameNr-1, 'rect_size'] = self.rect_size
+        self.repo.update_rectangle(self.video_id, self.current_frameNr, rel_x, rel_y, self.rect_size)
 
 if __name__ == "__main__":
     watch_predictions = False
     prediction_model = '../models/frames_skillborder_CNN_model_96pixels_history.pkl'
     video_id = 8
+    batch_size = 9999
     root = tk.Tk()
-    app = VideoLabeler(root, video_id)  # Adjust display width and height as needed
+    app = VideoLabeler(root, video_id, batch_size)  # Adjust display width and height as needed
     root.mainloop()
