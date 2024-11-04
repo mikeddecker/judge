1defc563502a10b9a1fd626128934903adf7d96b
Mike De Decker <mike.dedecker@student.hogent.be>
2024-11-04 11:38:01 +0100

    web vue init browse page

---
+++ web/src/views/BrowseView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="browse">
+    <h1>This is the video browse page</h1>
+
+    
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .browse {
+    margin: 0 2rem;
+    /* min-height: 100vh;
+    display: flex;
+    align-items: center; */
+  }
+}
+</style>
