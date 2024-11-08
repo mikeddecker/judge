bf547594e9f6c7a1e3138e3cbac3028487cc88d6
Mike De Decker <mike.dedecker@student.hogent.be>
2024-11-08 17:07:14 +0100

    web update browse view

--- web/src/views/BrowseView.vue
+++ web/src/views/BrowseView.vue
@@ -2,7 +2,11 @@
   <div class="browse">
     <h1>This is the video browse page</h1>
 
-    
+    <div class="flexcontainer">
+      <img class="timet" src="/home/miked/Pictures/Screenshots/timetable.png"/>
+      <img class="timet" src="/home/miked/Pictures/Screenshots/Screenshot from 2024-10-10 17-36-38.png"/>
+      <img class="timet" src="/home/miked/Pictures/Screenshots/Screenshot from 2024-10-11 10-46-17.png"/>
+    </div>
   </div>
 </template>
 
@@ -13,6 +17,12 @@
     /* min-height: 100vh;
     display: flex;
     align-items: center; */
+  }
+  .flexcontainer {
+    display: flex
+  }
+  .timet {
+    width: 30rem;
   }
 }
 </style>
