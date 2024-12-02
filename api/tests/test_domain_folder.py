976ea2443fc0e7378cf1ba92efc0af3a5b9707dc
Mike De Decker <mike.dedecker@student.hogent.be>
2024-12-02 23:12:41 +0100

    API - test.domain.folder rename methods ctor

--- api/tests/test_domain_folder.py
+++ api/tests/test_domain_folder.py
@@ -16,7 +16,7 @@ class DomainFolderTestSuite(unittest.TestCase):
         (125, "free", "main"),
         (125, "competition", None),
     ])
-    def test_create_folder_valid(self, id, name, parentname):
+    def test_ctor_valid(self, id, name, parentname):
         folderparent = Folder(id, parentname, None) if parentname else None
         folder = Folder(id, name, folderparent)
         self.assertEqual(name, folder.Name, f"Foldername incorrectly initialized {name}, {folder.Name}")
@@ -24,17 +24,17 @@ class DomainFolderTestSuite(unittest.TestCase):
         self.assertEqual(folderparent, folder.Parent, f"Parent of folder incorrectly initialized")
 
     @parameterized.expand(generate_empty_strings())
-    def test_create_folder_invalid_name_none(self, name):
+    def test_ctor_invalid_name_none(self, name):
         with self.assertRaises(ValueError):
             Folder(1, name, None)
 
     @parameterized.expand([None, 0, -1, -55])
-    def test_create_folder_invalid_id(self, id):
+    def test_ctor_invalid_id(self, id):
         with self.assertRaises(ValueError):
             Folder(id, "competition", None)
 
     @parameterized.expand([7, "text", ValueError])
-    def test_create_folder_invalid_parent_not_a_folder(self, parent):
+    def test_ctor_invalid_parent_not_a_folder(self, parent):
         with self.assertRaises(ValueError):
             Folder(1, "competition", parent)
 
