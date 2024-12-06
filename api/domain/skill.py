fd75872c33c6aad81585f9fa967f4cf8c7ed93f5
Mike De Decker <mike.dedecker@student.hogent.be>
2024-12-06 22:58:43 +0100

    API - domain.skill, test.domain.skill basics

--- api/domain/skill.py
+++ api/domain/skill.py
@@ -1,2 +1,31 @@
+
 class Skill:
-    pass
+    FrameStart = int
+    FrameEnd = int # Included
+
+    def __init__(self, id: int, dottedName: str, frameStart: int = None, frameEnd: int = None):
+        self.__setId(id)
+        self.__setDottedName(dottedName)
+
+    def __setattr__(self, name, value):
+        if hasattr(self, name):
+            if name == 'Id':
+                self.__setId(value)
+            # Prevent setting immutable attributes after it is set in __init__
+            if name in ["Name", "Folder"]:
+                raise AttributeError(f"Cannot modify '{name}' once it is set")
+        super().__setattr__(name, value)
+
+    def __setId(self, id):
+        if hasattr(self, 'Id') and self.Id is not None:
+            raise AttributeError(f"Cannot modify Id once it is set")
+        if id is None or id <= 0:
+            raise ValueError("Id must be strict positive")
+        self.Id = id
+
+    def __setDottedName(self, dottedName:str):
+        if hasattr(self, 'DottedName') and self.DottedName is not None:
+            raise AttributeError(f"Cannot modify DottedName once it is set")
+        if not dottedName or dottedName.isspace():
+            raise ValueError("DottedName may not be an empty string")
+        self.DottedName = dottedName
---
+++ api/tests/test_domain_skill.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import unittest
+from parameterized import parameterized
+from domain.folder import Folder
+from domain.videoinfo import VideoInfo
+from domain.skill import Skill
+
+def generate_empty_strings():
+    return [ 
+        None, "", " ", "\n", "\r", "  ", "\t ", "\t", " \r\n "
+    ]
+
+FOLDER_INSTANCE_VALID = Folder(1, 'competition', None)
+
+# TODO : check tests of whole class
+
+class DomainSkillTestSuite(unittest.TestCase):
+    """Domain folder test cases."""
+    @parameterized.expand([
+        (1, "QU.s.EB.AS.o"),
+        (2, "QU.s.EB.AS.o"),
+        (987, "QU.s.EB.AS.o"),
+        (1, "Gym.cartwheel"),
+        (1, "R.1h"),
+    ])
+    def test_ctor_valid(self, id, dottedName):
+        pass
+    
+    @parameterized.expand(generate_empty_strings())
+    def test_ctor_invalid_name(self, dottedName):
+        with self.assertRaises(ValueError):
+            Skill(1, dottedName)
+
+    @parameterized.expand([None, 0, -1, -55])
+    def test_ctor_invalid_id(self, id):
+        with self.assertRaises(ValueError):
+            Skill(id, "toad", None)
+
+    def test_change_id_immutable(self):
+        with self.assertRaises(AttributeError):
+            video = Skill(1, "toad")
+            video.Id = 7
+
+    def test_change_name_immutable(self):
+        with self.assertRaises(AttributeError):
+            video = Skill(1, "toad", None)
+            video.Name = "elephant"
+
+    def test_change_id_private_method(self):
+        with self.assertRaises(AttributeError):
+            video = VideoInfo(1, "toad", None)
+            video.__setId(88)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
