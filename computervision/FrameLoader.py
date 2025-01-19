dda91604032911265076c93be64c29294f6cceb3
Mike De Decker <mike.dedecker@student.hogent.be>
2025-01-19 18:12:57 +0100

    cv - localize data augmentation

--- computervision/FrameLoader.py
+++ computervision/FrameLoader.py
@@ -1,7 +1,9 @@
 import cv2
-import os
+import math
 import numpy as np
+import os
 import sys
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,8 +15,7 @@ class FrameLoader:
         self.VideoNames = datarepo.VideoNames
         self.VideoNames.index = self.VideoNames["id"]
 
-    def get_frame(self, videoId, frameNr, dim, original_x, original_y, original_width, original_height, printId=False):
-        # print(original_x, original_y, original_width, original_height)
+    def get_frame_original(self, videoId, frameNr, dim, original_x, original_y, original_width, original_height, printId=False):
         if printId:
             print(frameNr)
         vpath = os.path.join(STORAGE_DIR, self.VideoNames.loc[videoId, "name"])
@@ -26,6 +27,7 @@ class FrameLoader:
         scaled_height = dim if height == max_wh else int(dim * height / max_wh)
         cap.set(cv2.CAP_PROP_POS_FRAMES, frameNr)
         _, frame = cap.read()
+
         frame = cv2.resize(frame, (scaled_width, scaled_height), interpolation=cv2.INTER_AREA)
 
         zeros = np.zeros((dim, dim, 3), dtype=np.uint8)
@@ -42,6 +44,109 @@ class FrameLoader:
         y = [offset_x, offset_y, original_width * scale_x, original_height * scale_y]
         cap.release()
         return zeros, y
+    
+    def get_frame(self, videoId, frameNr, dim, original_x, original_y, original_width, original_height, printId=False):
+        if printId:
+            print(frameNr)
+        vpath = os.path.join(STORAGE_DIR, self.VideoNames.loc[videoId, "name"])
+        cap = cv2.VideoCapture(vpath)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        x1 = math.floor(original_x * width - width * original_width / 2)
+        x2 = math.ceil(original_x * width + width * original_width / 2)
+        y1 = math.floor(original_y * height - height * original_height / 2)
+        y2 = math.ceil(original_y * height + height * original_height / 2)
+
+        top_height = y1
+        bottom_height = height - y2
+        left_width = x1
+        right_width = width - x2
+        cut_top = int(math.pow(random.random(), 4) / 2 * top_height)
+        cut_bottom = int(math.pow(random.random(), 4) / 2 * bottom_height)
+        cut_left = int(math.pow(random.random(), 4) / 2 * left_width)
+        cut_right = int(math.pow(random.random(), 4) / 2 * right_width)
+
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frameNr)
+        _, frame = cap.read()
+        frame = frame[cut_top:height-cut_bottom, cut_left:width - cut_right]
+        height = frame.shape[0]
+        width = frame.shape[1]
+        x1 = x1 - cut_left
+        x2 = x2 - cut_left
+        y1 = y1 - cut_top
+        y2 = y2 - cut_top
+        box_width = x2 - x1
+        box_height = y2 - y1
+
+        if box_width > height or box_height > width:
+            zeros = np.zeros((max(width, height), max(width, height), 3), dtype=np.uint8)
+            left_pad = 0 if width == max(width, height) else abs(height - width) // 2
+            top_pad = 0 if height == max(width, height) else abs(height - width) // 2
+            zeros[top_pad:top_pad+height, left_pad:left_pad+width] = frame
+            height = zeros.shape[0]
+            width = zeros.shape[1]
+            frame = zeros
+            x1 = x1 + left_pad
+            x2 = x2 + left_pad
+            y1 = y1 + top_pad
+            y2 = y2 + top_pad
+        else:
+            horizontal_margin =  min(height, width) - box_width
+            vertical_margin = min(height, width) - box_height
+            assert horizontal_margin >= 0
+            assert vertical_margin >= 0
+            multiplier = random.random()
+            assert multiplier <= 1 and multiplier >= 0
+            left_shift = max(0, int(x1 - random.random() * horizontal_margin)) if min(height,width) == height else 0
+            top_shift = max(0, int(y1 - random.random() * vertical_margin)) if min(height,width) == width else 0
+            left_shift = min(left_shift, width - min(height, width))
+            top_shift = min(top_shift, height - min(height, width))
+            assert top_shift + min(height, width) - top_shift == left_shift+min(height, width) - left_shift
+            assert left_shift+min(height, width) <= width
+            assert top_shift+min(height, width) <= height
+            frame = frame[top_shift:top_shift + min(height, width), left_shift: left_shift+min(height, width)]
+            assert frame.shape[0] == frame.shape[1]
+            x1 = x1 - left_shift
+            x2 = x2 - left_shift
+            y1 = y1 - top_shift
+            y2 = y2 - top_shift
+
+        # Random padding around
+        r = random.random()
+        if r < 0.2:
+            bigger_dim = int((1 + r) * frame.shape[0])
+            shift = int(frame.shape[0] / 2 * r)
+            zeros = np.zeros((bigger_dim, bigger_dim, 3), dtype=np.uint8)
+            zeros[shift:shift+frame.shape[0], shift:shift+frame.shape[1]] = frame
+            frame = zeros
+
+            x1 += shift
+            x2 += shift
+            y1 += shift
+            y2 += shift
+        
+        # Random flip
+        if random.random() < 0.5:
+            frame = cv2.flip(frame, 1)
+            x1 = frame.shape[0] - x1
+            x2 = frame.shape[0] - x2
+
+        center_x = (x2 + x1) / 2
+        center_y = (y2 + y1) / 2
+        x = center_x / frame.shape[1]
+        y = center_y / frame.shape[0]
+
+        box_width = x2 - x1
+        box_height = y2 - y1
+
+        w = box_width / frame.shape[1]
+        h = box_height / frame.shape[0]
+
+        frame = cv2.resize(frame, (dim, dim), interpolation=cv2.INTER_AREA)
+        
+        cap.release()
+        return frame, [x, y, w, h]
     
     def get_frames(self, videoId, frameInfo, dim, printId=False):
         # On sampling: frameInfo = { 'frameNr' : (x,y,w,h) }
