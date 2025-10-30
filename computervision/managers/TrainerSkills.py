bd97b9205bf7ab06f5be0a3d5b1ed551e5c201fe
Mike De Decker <mikeddecker@hotmail.com>
2025-10-30 23:11:49 +0100

    cv - error message on new default best weights

--- computervision/managers/TrainerSkills.py
+++ computervision/managers/TrainerSkills.py
@@ -173,13 +173,16 @@ class TrainerSkills:
                     best_model_name = best_model_stats['modelname']
                     best_model_backbone_output_neurons = PYTORCH_MODELS_SKILLS_TEST[best_model_name].get_output_feature_dim(recipe)
                     best_head = OutputHeadRecognition(best_model_backbone_output_neurons, df_layers, df_composition, max_instances_per_role, prop_counts)
-                    model: torch.nn.Module = PYTORCH_MODELS_SKILLS[best_model_name](head=best_head, recipe=RECIPES['SKILL'][best_model_stats['recipe']['name']]).to(device)
-                    model.load_state_dict(torch.load(pathBest, weights_only=True))
-                    optimizer = optim.Adam(model.parameters(), lr=recipe.learning_rate)
-                    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=scheduler_patience, factor=0.2)
-                    print(f"Re-evaluate best of best model ({best_model_name}), to get the most optimal comparisons")
-                    best_model_revalidation_results = validate(model=model, dataloader=dataloaderVal, optimizer=optimizer)
-                    print(f"{Fore.YELLOW}Target best ({best_model_name}) - {best_model_revalidation_results['f1_total_avg']:.4f}{Style.RESET_ALL}")
+                    try:                        
+                        model: torch.nn.Module = PYTORCH_MODELS_SKILLS[best_model_name](head=best_head, recipe=RECIPES['SKILL'][best_model_stats['recipe']['name']]).to(device)
+                        model.load_state_dict(torch.load(pathBest, weights_only=True))
+                        optimizer = optim.Adam(model.parameters(), lr=recipe.learning_rate)
+                        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=scheduler_patience, factor=0.2)
+                        print(f"Re-evaluate best of best model ({best_model_name}), to get the most optimal comparisons")
+                        best_model_revalidation_results = validate(model=model, dataloader=dataloaderVal, optimizer=optimizer)
+                        print(f"{Fore.YELLOW}Target best ({best_model_name}) - {best_model_revalidation_results['f1_total_avg']:.4f}{Style.RESET_ALL}")
+                    except:
+                        print(f"{Fore.RED}Error loading (weigths of) best model.{Style.RESET_ALL} Highly likely because of new parameters, this run becomes default best")
 
                 if not from_scratch and os.path.exists(path):
                     model: torch.nn.Module = PYTORCH_MODELS_SKILLS[modelname](head=head, recipe=recipe).to(device)
